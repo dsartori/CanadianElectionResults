6d60dc2f360bb1c19c6541233bcbc8a1ce9ccb52
Douglas Sartori <doug@sartori.ca>
2020-10-11 08:21:24 -0400

    add table-valued function for province and abbreviation data fixes add queries for vote share

--- DatabaseFiles/VoteShareByProvince.sql
+++ DatabaseFiles/VoteShareByProvince.sql
@@ -13,7 +13,6 @@ group by e.EcNameEn
 
  select 
 	pro.Province,
-	ed.ECNameEn,
 	pa.PartyNameEn,
 	sum(CASE WHEN ed.ElectionYear = 2015 THEN cast(Votes as decimal(10,6)) ELSE 0 END) / max(v.Votes2015) as 'Vote Share 2015',
 	sum(CASE WHEN ed.ElectionYear = 2019 THEN cast(Votes as decimal(10,6)) ELSE 0 END) / max(v.Votes2019) as 'Vote Share 2019',
@@ -26,7 +25,7 @@ from PollResult pr
  inner join ED_province() pro on ED.EDID = pro.EDID
  inner join #VotesCast v on ed.ECNameEn = v.ECNameEn
 where Pa.PartyNameEn in ('Conservative','Liberal','NDP-New Democratic Party','Bloc Québécois','Green Party')
-group by pro.province,ed.ECNameEn,pa.PartyNameEn
+group by pro.province,pa.PartyNameEn
 order by sum(CASE WHEN ed.ElectionYear = 2019 THEN cast(Votes as decimal(10,6)) ELSE 0 END) / max(v.Votes2019) - sum(CASE WHEN ed.ElectionYear = 2015 THEN cast(Votes as decimal(10,6)) ELSE 0 END) / max(v.Votes2015)
 desc
 
